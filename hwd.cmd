cc0ab2679641c70b7ed6052f5303023ed19dca11
n1creator <dev@n1creator.com>
2021-01-08 14:18:03 +0300

    fix

--- hwd.cmd
+++ hwd.cmd
@@ -11,63 +11,79 @@ IF "%1" EQU "" (
     IF "%1" EQU "compose" (
         IF NOT "%2" EQU "" (
             docker-compose -f "%~dp0\docker-compose.yml" --env-file "%~dp0\.env" %2 %3 %4 %5 %6 %7
+			exit /B 1
         )
     )
     IF "%1" EQU "down" (
-        docker-compose -f "%~dp0\docker-compose.yml" --env-file "%~dp0\.env" down
+        docker-compose -f "%~dp0\docker-compose.yml" --env-file "%~dp0\.env" down --remove-orphans
+		exit /B 1
     )
     IF "%1" EQU "recreate" (
         docker-compose -f "%~dp0\docker-compose.yml" --env-file "%~dp0\.env" up -d --build --force-recreate --remove-orphans
+		exit /B 1
     )
     IF "%1" EQU "watch" (
         docker-compose -f "%~dp0\docker-compose.yml" --env-file "%~dp0\.env" run --rm node npm run watch-poll
+		exit /B 1
     )
     IF "%1" EQU "dev" (
         docker-compose -f "%~dp0\docker-compose.yml" --env-file "%~dp0\.env" run --rm node npm run dev
+		exit /B 1
     )
     IF "%1" EQU "prod" (
         docker-compose -f "%~dp0\docker-compose.yml" --env-file "%~dp0\.env" run --rm node npm run prod
+		exit /B 1
     )
     IF "%1" EQU "npm" (
         IF NOT "%2" EQU "" (
             docker-compose -f "%~dp0\docker-compose.yml" --env-file "%~dp0\.env" run --rm node %*
+			exit /B 1
         )
     )
     IF "%1" EQU "node" (
         IF NOT "%2" EQU "" (
             docker-compose -f "%~dp0\docker-compose.yml" --env-file "%~dp0\.env" run --rm node %*
+			exit /B 1
         )
     )
     IF "%1" EQU "composer" (
         IF NOT "%2" EQU "" (
             docker-compose -f "%~dp0\docker-compose.yml" --env-file "%~dp0\.env" exec php %*
+			exit /B 1
         )
     )
     IF "%1" EQU "artisan" (
         IF NOT "%2" EQU "" (
             docker-compose -f "%~dp0\docker-compose.yml" --env-file "%~dp0\.env" exec php php %*
+			exit /B 1
         )
     )
     IF "%1" EQU "logs" (
         IF NOT "%2" EQU "" (
             docker-compose logs -f %2
+			exit /B 1
         )
     )
     IF "%1" EQU "deploy" (
         plink -batch n1creator@84.201.185.138 -i "d:\Projects\access&cfg\ssh-private.ppk" "cd /var/www/handswork.pro && sudo bash ./deploy.sh -gr -gp -gm -d %args%"
         plink -batch n1creator@84.201.185.133 -i "d:\Projects\access&cfg\ssh-private.ppk" "cd /var/www/handswork.pro && sudo bash ./deploy.sh -gr -gp %args%"
+		exit /B 1
     )
     IF "%1" EQU "server_1" (
-        plink -batch n1creator@84.201.185.138 -i "d:\Projects\access&cfg\ssh-private.ppk" "%args%"
+        plink -batch n1creator@84.201.185.138 -i "d:\Projects\access&cfg\ssh-private.ppk" "cd /var/www/handswork.pro && %args%"
+		exit /B 1
     )
     IF "%1" EQU "server_2" (
-        plink -batch n1creator@84.201.185.133 -i "d:\Projects\access&cfg\ssh-private.ppk" "%args%"
+        plink -batch n1creator@84.201.185.133 -i "d:\Projects\access&cfg\ssh-private.ppk" "cd /var/www/handswork.pro && %args%"
+		exit /B 1
     )
     IF "%1" EQU "server_db" (
         plink -batch n1creator@84.201.185.42 -i "d:\Projects\access&cfg\ssh-private.ppk" "%args%"
+		exit /B 1
     )
     IF "%1" EQU "hosts" (
         hwhosts
+		exit /B 1
     )
 )
 
